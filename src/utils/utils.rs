e0911c373d05069861f2b9114c4799a9210695d9
Damoon Azarpazhooh <damoon.azarpazhooh@live.com>
2018-09-09 22:24:31 -0400

    done : util::to_utf8()

--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -1,6 +1,4 @@
-// var padLeft = function (string, chars, sign) {
-//     return new Array(chars - string.length + 1).join(sign ? sign : "0") + string;
-// };
+use std::str;
 pub fn pad_left(input : String,shift : usize , sign_bit : String) -> String{
     let mut result = input.clone();
     let mut padding : &str = &sign_bit;
@@ -24,3 +22,28 @@ pub fn pad_right(input : String,shift : usize , sign_bit : String) -> String{
     }
     result
 }
+
+pub fn to_utf8(input : String) -> String{
+    let mut result : String = String::new();
+    let mut input_clone = input.clone();
+    if input.len() >= 2 {          
+            if &input[0..2] == "0x" {
+                input_clone = (&input[2..input.len()]).to_string();      
+            }
+            if input_clone.len()%2 == 1 {
+                input_clone = (&input_clone[0..input_clone.len()-1]).to_string();  
+            }
+            let mut i = 0 ; 
+            while i<=input_clone.len()-2 {
+                let temp = u8::from_str_radix(&input_clone[i..i+2], 16).map(|n| n as char);
+                if temp.is_ok(){
+                    result.push(temp.ok().unwrap());
+                }
+                i += 2;
+            };
+    };
+   
+    result
+}
+// u8::from_str_radix("A", 16), Ok(10)
+//          n as char
--- src/utils/utils_test.rs
+++ src/utils/utils_test.rs
@@ -34,5 +34,46 @@ pub mod utils_test {
         assert_eq!(case, result);
 
     }
-
+    #[test]
+    #[ignore]
+    fn to_utf8(){
+        let case = "JKL".to_string();
+        let result = utils::to_utf8("4a4b4c".to_string());
+        assert_eq!(case, result);
+    }
+    #[test]
+    #[ignore]
+    fn to_utf8_alt(){
+        let case = "J".to_string();
+        let result = utils::to_utf8("4aF".to_string());
+        assert_eq!(case, result);
+    }
+    #[test]
+    #[ignore]
+    fn to_utf8_with_0x(){
+        let case = "JKL".to_string();
+        let result = utils::to_utf8("0x4a4b4c".to_string());
+        assert_eq!(case, result);
+    }
+    #[test]
+    #[ignore]
+    fn to_utf8_odd_len(){
+        let case = "JK".to_string();
+        let result = utils::to_utf8("0x4a4b4".to_string());
+        assert_eq!(case, result);
+    }
+    #[test]
+    #[ignore]
+   fn to_utf8_empty(){
+        let case =String::new();
+        let result = utils::to_utf8(String::new());
+        assert_eq!(case, result);
+    }
+    #[test]    
+    #[ignore]
+    fn to_utf8_smaller_than_2_len(){
+        let case = String::new();
+        let result = utils::to_utf8("1".to_string());
+        assert_eq!(case, result);
+    }
 }
